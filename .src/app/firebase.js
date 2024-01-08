b7f7764a6262727379f6e3930bb86281535c3093
frank_henrry <frankhenrrybustillosbenancio@gmail.com>
2024-01-08 18:27:42 -0500

    version_2

--- .src/app/firebase.js
+++ .src/app/firebase.js
@@ -3,12 +3,8 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"
 //credenciales
 const firebaseConfig = {
-  apiKey: "AIzaSyDxkz7Hqq_S-8w94keyGir2zwQIkxd-R0k",
-  authDomain: "prueba-feb52.firebaseapp.com",
-  projectId: "prueba-feb52",
-  storageBucket: "prueba-feb52.appspot.com",
-  messagingSenderId: "473928079118",
-  appId: "1:473928079118:web:615586273ef6b08db5695f"
+  
+  
 };
 
 export const app = initializeApp(firebaseConfig);//inicializacion
